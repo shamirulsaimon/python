59ac855ad1ffefd4ce9fbef22f1e668913552fda
Samirul Rahman Saimon <shamirulrhaman@gmail.com>
2024-10-06 01:34:45 +0600

    fix minor issues and finish the game

--- Hangman with Python/main.py
+++ Hangman with Python/main.py
@@ -1,15 +1,83 @@
 import random
-word_list = ["aardvark","baboon","camel"]
 
+stages = [
+    """
+       -----
+       |   |
+       O   |
+      /|\\  |
+      / \\  |
+           |
+    =========
+    """,
+    """
+       -----
+       |   |
+       O   |
+      /|\\  |
+      /    |
+           |
+    =========
+    """,
+    """
+       -----
+       |   |
+       O   |
+      /|\\  |
+           |
+           |
+    =========
+    """,
+    """
+       -----
+       |   |
+       O   |
+      /|   |
+           |
+           |
+    =========
+    """,
+    """
+       -----
+       |   |
+       O   |
+       |   |
+           |
+           |
+    =========
+    """,
+    """
+       -----
+       |   |
+       O   |
+           |
+           |
+           |
+    =========
+    """,
+    """
+       -----
+       |   |
+           |
+           |
+           |
+           |
+    =========
+    """
+]
+
+word_list = ["aardvark", "baboon", "camel"]
+
+lives = 6
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
+print(chosen_word)  # For testing purposes
 
 placeholder = ""
 
-word_lenght = len(chosen_word)
-for position in range(word_lenght):
- placeholder += "_"
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
 
 print(placeholder)
 
@@ -18,21 +86,31 @@ correct_letter = []
 
 while not game_over:
 
- guess = input("Player Guess a letter: ").lower()
+    guess = input("Player, guess a letter: ").lower()
 
- display = ""
+    display = ""
 
- for letter in chosen_word:
-  if letter == guess:
-   display += letter
-   correct_letter.append(letter)
-  elif letter in correct_letter:
-   display+=letter
-  else:
-   display += "_"
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(letter)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
 
- print(display)
+    print(display)
 
- if "_" not in display:
-  game_over = True
-  print("You Win the game")
+    # This condition checks if the guess is not in the word.
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Wrong guess! You have {lives} lives left.")
+        if lives == 0:
+            print("Game over. You lose!")
+            game_over = True
+
+    if "_" not in display:
+        game_over = True
+        print("You win the game!")
+
+    print(stages[lives])
